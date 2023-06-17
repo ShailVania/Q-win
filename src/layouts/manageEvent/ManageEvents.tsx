d2cbae18cd89e683cb951e2ddb57863141d4a947
Shail Vania <vanias@uwindsor.ca>
2023-06-17 10:01:50 -0400

    Resolved bugs for admin dashboards

--- src/layouts/manageEvent/ManageEvents.tsx
+++ src/layouts/manageEvent/ManageEvents.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react"
 import { toastr } from "react-redux-toastr"
 import { useParams } from "react-router-dom"
-import { useAppDispatch, useAppSelector } from "../../store/store"
+import { useAppDispatch, useAppSelector } from "../../../../hey/qwin/src/store/store"
 import Header from "../header/Header"
-import "./manageEvent.css"
+import NewHeader from "../header/NewHeader"
 import {
   deleteEvent,
   EventDetails,
@@ -13,6 +13,7 @@ import {
   updateEvent,
   getEventFunction,
 } from "./manageEventSlice"
+import UsersTable from "./UsersTable"
 
 export default function ManageEvents() {
   const [event, setEvent] = useState<EventDetails>(initialEventDetails)
@@ -21,6 +22,11 @@ export default function ManageEvents() {
   const disableCloseButtonFocus = true
   const { id } = useParams()
 
+  useEffect(() => {
+    return () => {
+      setEvent(initialEventDetails)
+    }
+  }, [])
   // fetch details of event from FireBase
   useEffect(() => {
     if (id) {
@@ -30,85 +36,123 @@ export default function ManageEvents() {
 
   // update state object from redux
   useEffect(() => {
-    if (!event.id && reduxEvent.id) {
+    if (!window.location.href.includes("/create-event")) {
       setEvent(reduxEvent)
       console.log("Redux Event: ", reduxEvent)
     }
+    return () => {}
   }, [reduxEvent])
 
   return (
-    <React.Fragment>
-      <Header />
-      <div className="event-form">
-        <h1 className="form-title">Create Event</h1>
-        <form>
-          <div className="form-group">
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              value={event.title}
-              onChange={(e) => setEvent({ ...event, title: e.target.value })}
-              required
-            />
+    <div>
+      <NewHeader />
+      <main style={{ position: "relative", top: "14em" }} className="main-content mt-0">
+        <section className="row">
+          <div className="col-6">
+            <div className="container mb-5">
+              <div className="row mt-lg-n10 mt-md-n11 mt-n12">
+                <div className="col-12 mx-auto">
+                  <div className="card z-index-0">
+                    <div>
+                      <div className="card-body">
+                        <h2>{event?.id ? "Update Event" : "Create Event"}</h2>
+                        <form
+                          onSubmit={(e) => {
+                            e.preventDefault()
+                            if (event.id) dispatch(updateEvent(event))
+                            else dispatch(createEvent(event))
+                          }}
+                        >
+                          <div className="mb-3">
+                            <label htmlFor="title" className="form-label">
+                              Title:
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="title"
+                              name="title"
+                              value={event.title}
+                              onChange={(e) => setEvent({ ...event, title: e.target.value })}
+                              required
+                            />
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="description" className="form-label">
+                              Description:
+                            </label>
+                            <textarea
+                              className="form-control"
+                              id="description"
+                              name="description"
+                              value={event.description}
+                              onChange={(e) => setEvent({ ...event, description: e.target.value })}
+                              required
+                            ></textarea>
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="address" className="form-label">
+                              Address:
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="address"
+                              name="address"
+                              value={event.address}
+                              onChange={(e) => setEvent({ ...event, address: e.target.value })}
+                              required
+                            />
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="date-time" className="form-label">
+                              Date and Time:
+                            </label>
+                            <input
+                              type="datetime-local"
+                              className="form-control"
+                              id="date-time"
+                              value={event.date}
+                              onChange={(e) => setEvent({ ...event, date: e.target.value })}
+                              name="date-time"
+                              required
+                            />
+                          </div>
+                          <div className="button-group">
+                            <button
+                              type="submit"
+                              className="create-button btn bg-gradient-dark w-100 my-4 mb-2"
+                              onClick={(e) => {
+                                // e.preventDefault()
+                                // if (event.id) dispatch(updateEvent(event))
+                                // else dispatch(createEvent(event))
+                              }}
+                            >
+                              Submit
+                            </button>
+                            <button
+                              type="button"
+                              className="cancel-button btn bg-gradient-danger w-100 my-4 mb-2"
+                              onClick={(e) => {
+                                dispatch(deleteEvent(event))
+                              }}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="description">Description:</label>
-            <textarea
-              id="description"
-              name="description"
-              value={event.description}
-              onChange={(e) => setEvent({ ...event, description: e.target.value })}
-              required
-            ></textarea>
+          <div className="col-6">
+            <UsersTable eventID={id} />
           </div>
-          <div className="form-group">
-            <label htmlFor="address">Address:</label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              value={event.address}
-              onChange={(e) => setEvent({ ...event, address: e.target.value })}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="date-time">Date and Time:</label>
-            <input
-              type="datetime-local"
-              id="date-time"
-              value={event.date}
-              onChange={(e) => setEvent({ ...event, date: e.target.value })}
-              name="date-time"
-              required
-            />
-          </div>
-          <div className="button-group">
-            <button
-              type="submit"
-              className="create-button"
-              onClick={(e) => {
-                e.preventDefault()
-                if (event.id) dispatch(updateEvent(event))
-                else dispatch(createEvent(event))
-              }}
-            >
-              Submit
-            </button>
-            <button
-              type="button"
-              className="cancel-button"
-              onClick={(e) => {
-                dispatch(deleteEvent(event))
-              }}
-            >
-              Delete
-            </button>
-          </div>
-        </form>
-      </div>
-    </React.Fragment>
+        </section>
+      </main>
+    </div>
   )
 }
